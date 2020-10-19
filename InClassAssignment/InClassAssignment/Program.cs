08ac14efd35211e0fd2b37b7849eaac831656c44
haritha <haritham18@gmail.com>
2020-10-19 19:53:53 -0400

    Added Inclass Assignment

---
+++ InClassAssignment/InClassAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate the sum of two numbers:");
+            string str = Console.ReadLine();
+            string[] value = str.Split('+');
+            int num1 = int.Parse(value[0]);
+            int num2 = int.Parse(value[1]);
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+        }
+    }
+}
